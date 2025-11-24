8ccf4678f289772b92c49d3f7c37c7c479012985
luizfritoli <luizgustavofritoli@gmail.com>
2025-11-24 11:25:44 -0300

    Lógica do jokenpo finalizada

--- src/components/jokenpo/Jokenpo.jsx
+++ src/components/jokenpo/Jokenpo.jsx
@@ -5,93 +5,107 @@ const Jokenpo = () => {
   const [isPlaying, setIsPlaying] = useState(false);
 
   // Armazenar as escolhas do computador e do usuário
-  const [playerChoice, setPlayerChoice] = useState(null)
-  const [computerChoice, setComputerChoice] = useState(null)
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
 
-  const [runner, setRunner] = useState(false)
+  const [runner, setRunner] = useState(false);
 
-  // Mostrar e destruir a mensagem da escolha do usuário
-  const [showMessage, setShowMessage] = useState(false)
+  // Mostrar e destruir a mensagem de escolha do usuário
+  const [showMessage, setShowMessage] = useState(false);
 
-  const [result, setResult] = useState(null)
-  const startJokenpoMatch = () => {
-    const options = ["Stone", "Paper", "Scissors"]
-    
-    setComputerChoice(options[Math.floor(Math.random() * options.length)])
-
-    console.log(computerChoice)
-    
-   if (
-  (playerChoice === "Stone" && computerChoice === "Scissors") ||
-  (playerChoice === "Paper" && computerChoice === "Stone") ||
-  (playerChoice === "Scissors" && computerChoice === "Paper")
-) {
-  setResult("Você venceu!")
-} else if (playerChoice === computerChoice) {
-  setResult("Empate!");
-} else {
-  setResult("Você perdeu!")
-}
-
-  }
+  const [winner, setWinner] = useState(null);
 
   const setChoice = (choice) => {
-    setPlayerChoice(choice)
-    setShowMessage(true)
-    setRunner(true)
-}
+    // Após a escolha, mostrar a mensagem de confirmação da mesma
+    setShowMessage(true);
 
+    // Escolha do computador
+    const options = ["Stone", "Paper", "Scissors"];
+    const calcPcChoice = options[Math.floor(Math.random() * options.length)];
+
+    // Condições de vitória
+    if (
+      (choice === "Stone" && calcPcChoice === "Scissors") ||
+      (choice === "Paper" && calcPcChoice === "Stone") ||
+      (choice === "Scissors" && calcPcChoice === "Paper")
+    ) {
+      setWinner("USER");
+    } else if (choice === calcPcChoice) {
+      setWinner("TIE");
+    } else {
+      setWinner("COMPUTER");
+    }
+
+    setPlayerChoice(choice);
+    setComputerChoice(calcPcChoice);
+    console.log(`${choice} | ${calcPcChoice}`);
+
+    setRunner(true);
+  };
+
+  // Resetar tudo ao jogar novamente
+  const playAgain = () => {
+    setPlayerChoice(null);
+    setComputerChoice(null);
+    setWinner(null);
+    setIsPlaying(false);
+  };
+
+  // UI da partida
   useEffect(() => {
-    const handler = setTimeout(()=> {
-      setRunner(false)
-    }, 4000)
+    const handler = setTimeout(() => {
+      setRunner(false);
+    }, 4000);
 
-    return () => clearTimeout(handler)
-  }, [runner])
+    return () => clearTimeout(handler);
+  }, [runner]);
 
+  // Visibilidade do texto de escolha
   useEffect(() => {
     const visibility = setTimeout(() => {
-      setShowMessage(false)
-      startJokenpoMatch()
-    }, 3000)
+      setShowMessage(false);
+    }, 3000);
 
-    return () => clearTimeout(visibility)
-  }, [showMessage])
+    return () => clearTimeout(visibility);
+  }, [showMessage]);
   return (
     <section className="flex flex-col justify-center items-center h-screen w-full">
-      <div className="h-[5%] flex justify-center items-center">
-        <h1 className="text-center font-bold text-[2em]">PEDRA, PAPEL E TESOURA</h1>
-      </div>
+      {/* Seção inicial do jogo */}
       <Activity mode={isPlaying ? "hidden" : "visible"}>
-        <div className="flex flex-col justify-center items-center h-[70%] w-full bg-green-700">
+        <div className="flex flex-col justify-center items-center h-[100%] w-full bg-green-700">
           <button
             type="button"
-            className="bg-green-600 text-[2.5em] p-4 cursor-pointer rounded-lg w-[6em] 
-            mb-[5em] text-[#FFF] font-medium"
+            className="bg-green-600 text-[2.5em] p-3 cursor-pointer rounded-lg w-[6em] 
+            mb-[4em] text-[#FFF] font-medium"
             onClick={() => setIsPlaying(true)}
           >
             JOGAR
           </button>
         </div>
       </Activity>
+      {/* Quando o usuário clicar em "JOGAR" */}
       <Activity mode={isPlaying && !playerChoice ? "visible" : "hidden"}>
-        <div className="flex flex-col justify-center items-center h-[70%] w-full bg-neutral-700">
-          <h2>FAÇA A SUA ESCOLHA!</h2>
-          <button type="button" onClick={() => setChoice("Stone")}>
-            Pedra
-          </button>
-          <button type="button" onClick={() => setChoice("Paper")}>
-            Papel
-          </button>
-          <button type="button" onClick={() => setChoice("Scissors")}>
-            Tesoura
-          </button>
+        <div className="flex flex-col justify-around items-center h-[100%] w-full bg-neutral-700">
+          <h2 className="text-[2em] text-[#FFF] font-semibold">FAÇA A SUA ESCOLHA!</h2>
+          <div className="flex flex-row gap-12 justify-center items-center">
+            {/* Escolhas */}
+            <button type="button" className="cursor-pointer" onClick={() => setChoice("Stone")}>
+              Pedra
+            </button>
+            <button type="button" className="cursor-pointer" onClick={() => setChoice("Paper")}>
+              Papel
+            </button>
+            <button type="button" className="cursor-pointer" onClick={() => setChoice("Scissors")}>
+              Tesoura
+            </button>
+          </div>
         </div>
       </Activity>
+      {/* Após a escolha do usuário */}
       <Activity mode={isPlaying && playerChoice ? "visible" : "hidden"}>
-        <div className="flex flex-col gap-12 justify-center items-center h-[70%] w-full bg-violet-300">
+        <div className="flex flex-col gap-12 justify-around items-center h-[100%] w-full bg-violet-300">
           {showMessage && (
-            <h2 className="">
+            <h2 className="absolute top-[40%]">
               Sua escolha foi{" "}
               {playerChoice === "Stone" ? "pedra" : playerChoice === "Paper" ? "papel" : "tesoura"}!
             </h2>
@@ -100,7 +114,27 @@ const Jokenpo = () => {
             <span>{runner ? "Escolhendo..." : playerChoice}</span>
             <span>VS</span>
             <span>{runner ? "Escolhendo..." : computerChoice}</span>
-            <span>{runner ? null : result}</span>
+          </div>
+          <div className="absolute top-[20%]">
+            <div className="flex flex-col justify-center items-center gap-8">
+              <span className="text-[2.4em]">
+                {runner
+                  ? null
+                  : winner === "USER"
+                    ? "Você venceu!"
+                    : winner === "COMPUTER"
+                      ? "O computador venceu!"
+                      : "Empate!"}
+              </span>
+              {!runner && winner && (
+                <button
+                  className="text-[1.1em] bg-[#A7E63A] rounded-lg font-medium p-3 cursor-pointer"
+                  onClick={playAgain}
+                >
+                  Jogar novamente
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </Activity>
